9ade4b340e331611eacdb71e0e13c0d002a99ac5
Denis omondi <126732649+Denisomondi@users.noreply.github.com>
2023-05-20 14:14:14 +0300

    Update book.rb
    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,25 @@
+class Book
+    # attr_reader :title
+    # attr_writer :title
+    # attr_reader :author
+    # attr_writer :author
+    # attr_reader :page_count
+    # attr_writer :page_count
+    # attr_reader :genre
+    # attr_writer :genre
+
+    # attr_reader :turn_page
+    attr_accessor :title, :author, :page_count, :genre, :turn_page
+
+    def initialize(title)
+        @title = title
+        
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!" 
+    end
+
+end
 
 
